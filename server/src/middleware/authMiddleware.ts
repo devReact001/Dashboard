147307db376d3ffc25d4fdb69165db534fe9e720
deepak-rgb-prog <deepak@recruitmentsmart.com>
2026-04-21 10:37:52 +0530

    server correction

--- server/src/middleware/authMiddleware.ts
+++ server/src/middleware/authMiddleware.ts
@@ -16,7 +16,7 @@ export const authMiddleware = (
   }
 
   try {
-    const decoded = jwt.verify(token, "SECRET_KEY");
+    const decoded = jwt.verify(token, process.env.JWT_SECRET!);
     (req as any).user = decoded;
 
     next();
